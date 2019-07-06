afe15e2e53b707a2a043aa12ae8ebd5fff804559
kagajr <trinhduykhang1313@gmail.com>
2019-07-06 12:06:05 +0700

    first commit

--- Program.cs
+++ Program.cs
@@ -4,8 +4,14 @@ namespace git_example
 {
     class Program
     {
+        private static bool IsCalling(bool ringing)
+        {
+            return ringing;
+        }
         static void Main(string[] args)
         {
+            var ring = "Iphone's ringtune";
+            IsCalling(ring != null);
             Console.WriteLine("Hello World!");
         }
     }

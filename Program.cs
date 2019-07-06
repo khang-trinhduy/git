a82923e26160dba3e7ae938ebaa4c7367c362ecd
kagajr <trinhduykhang1313@gmail.com>
2019-07-06 16:05:52 +0700

    split call

--- Program.cs
+++ Program.cs
@@ -4,8 +4,13 @@ namespace git_example
 {
     class Program
     {
+        private static void Call()
+        {
+            System.Console.WriteLine("MoshiMoshi!");
+        }
         static void Main(string[] args)
         {
+            Call();
             Console.WriteLine("Hello World!");
         }
     }

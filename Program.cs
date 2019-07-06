fba0f2b94c0575281642059d7c3491cec00affcb
kagajr <trinhduykhang1313@gmail.com>
2019-07-06 16:08:08 +0700

    remove call

--- Program.cs
+++ Program.cs
@@ -8,14 +8,9 @@ namespace git_example
         {
             return ringing;
         }
-
-        private static void Call()
-        {
-            System.Console.WriteLine("MoshiMoshi!");
-        }
         static void Main(string[] args)
         {
-            Call();var ring = "Iphone's ringtune";
+            var ring = "Iphone's ringtune";
             IsCalling(ring != null);
             Console.WriteLine("Hello World!");
         }
